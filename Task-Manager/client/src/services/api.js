a948935374a013a92201f5fbdc8f7fa48a53afef
Srishti Mishra <srishtimishra2411@gmail.com>
2026-05-22 10:43:58 +0530

    fix api integration with env

--- Task-Manager/client/src/services/api.js
+++ Task-Manager/client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080"
+    // Hardcode your production API path directly as the fallback
+    baseURL: import.meta.env.VITE_API_URL || "https://task-manager-production-a581.up.railway.app/api"
 });
 
 api.interceptors.request.use((config) => {
